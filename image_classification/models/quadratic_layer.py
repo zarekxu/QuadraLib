9378ac20f49d4830108f93deb805033e81caed8a
zarekxu <31581108+zarekxu@users.noreply.github.com>
2022-01-28 14:22:59 -0500

    Dev (#4)
    * test
    
    * vgg_cifar10

--- image_classification/models/quadratic_layer.py
+++ image_classification/models/quadratic_layer.py
@@ -1,13 +1,13 @@
 import torch.nn as nn
 import torch.nn.functional as F
-
+import torch
 
 class Quadraour(nn.Module):
-	def __init__(self, in_channels, out_channels, kernel_size, stride, padding, dialation):
+	def __init__(self, in_channels, out_channels, kernel_size, stride, padding, dilation):
 		super(Quadraour, self).__init__()
-		self.conv1 = nn.Conv2d(in_channels, out_channels,kernel_size, stride=1, padding=1, dialation=1)
-		self.conv2 = nn.Conv2d(in_channels, out_channels,3,stride=1, padding=1)
-		self.conv3 = nn.Conv2d(in_channels, out_channels,3,stride=1, padding=1)
+		self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size , stride, padding, dilation)
+		self.conv2 = nn.Conv2d(in_channels, out_channels, kernel_size , stride, padding, dilation)
+		self.conv3 = nn.Conv2d(in_channels, out_channels, kernel_size , stride, padding, dilation)
 		self.bn1 = nn.BatchNorm2d(out_channels)
 		self.bn2 = nn.BatchNorm2d(out_channels)
 		self.bn3 = nn.BatchNorm2d(out_channels)
@@ -24,35 +24,10 @@ class Quadraour(nn.Module):
 		y = y + y2
 		return y
 
-# class Quadraourdropout(nn.Module):
-# 	def __init__(self, in_channels, out_channels):
-# 		super(Quadraourdropout, self).__init__()
-# 		self.conv1 = nn.Conv2d(in_channels, out_channels,3,stride=1, padding=1)
-# 		self.conv2 = nn.Conv2d(in_channels, out_channels,3,stride=1, padding=1)
-# 		self.conv3 = nn.Conv2d(in_channels, out_channels,3,stride=1, padding=1)
-# 		self.dropout = nn.Dropout(0.25)
-# 		self.bn1 = nn.BatchNorm2d(out_channels)
-# 		self.bn2 = nn.BatchNorm2d(out_channels)
-# 		self.bn3 = nn.BatchNorm2d(out_channels)
-        
-# 	def forward(self,x):
-        
-# 		y0 = self.conv1(x)
-# 		# y0 = self.bn1(y0)
-# 		y0 = self.dropout(y0)
-# 		y1 = self.conv2(x)
-# 		# y1 = self.bn1(y1)
-# 		y1 = self.dropout(y1)
-# 		y2 = self.conv3(x)
-# 		y = torch.mul(y0,y1)
-# 		y = y + y2
-# 		return y
-
-
 
 class Type1(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1):
-        super(Quadratype1, self).__init__()
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding, dilation):
+        super(Type1, self).__init__()
         self.in_channels = in_channels
         self.out_channels = out_channels
         self.kernel_size = kernel_size
@@ -85,9 +60,9 @@ class Type1(nn.Module):
 
 
 class Type3(nn.Module):
-	def __init__(self, in_channels, out_channels):
-		super(Quadratype3, self).__init__()
-		self.conv1 = nn.Conv2d(in_channels, out_channels,3,stride=1, padding=1)
+	def __init__(self, in_channels, out_channels, kernel_size, stride, padding, dilation):
+		super(Type3, self).__init__()
+		self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, dilation)
         
 	def forward(self,x):
 		print("aaaaa", x.size())
@@ -96,10 +71,10 @@ class Type3(nn.Module):
 
 
 
-class Quadratype2(nn.Module):
-	def __init__(self, in_channels, out_channels):
-		super(Quadratype2, self).__init__()
-		self.conv1 = nn.Conv2d(in_channels, out_channels,3,stride=1, padding=1)
+class Type2(nn.Module):
+	def __init__(self, in_channels, out_channels, kernel_size, stride, padding, dilation):
+		super(Type2, self).__init__()
+		self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, dilation)
         
 	def forward(self,x):
         
@@ -107,11 +82,11 @@ class Quadratype2(nn.Module):
 		return y
 
 
-class Quadratype4(nn.Module):
-	def __init__(self, in_channels, out_channels):
-		super(Quadratype4, self).__init__()
-		self.conv1 = nn.Conv2d(in_channels, out_channels,3,stride=1, padding=1)
-		self.conv2 = nn.Conv2d(in_channels, out_channels,3,stride=1, padding=1)
+class Type4(nn.Module):
+	def __init__(self, in_channels, out_channels, kernel_size, stride, padding, dilation):
+		super(Type4, self).__init__()
+		self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, dilation)
+		self.conv2 = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, dilation)
         
 	def forward(self,x):
         
